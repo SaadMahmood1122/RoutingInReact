afa6df8b4ae6c3ac50ae90f51766b580cec49e6c
SaadMahmood1122 <sk8066709@gamil.com>
2025-02-10 19:26:05 +0500

    added useParam hook

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductPage /> },
-      { path: "/products/:id", element: <ProductDetails /> },
+      { path: "/products/:productId", element: <ProductDetails /> },
     ],
   },
 ]);
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
 function ProductDetails() {
+  const param = useParams();
   return (
     <>
       <h1>Product Details page</h1>
+      <h2>Product id is {param.productId}</h2>
     </>
   );
 }
